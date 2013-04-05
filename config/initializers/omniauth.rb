17f2df846cd22df513eb10cff55d6cee95c205dc
Neel <neel@ubuntu.(none)>
2013-04-05 14:05:45 -0500

    trying to get facebook app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,'473713722693960','8c1f54c3d570a78fe6d325ab8401f487'
+  provider :facebook,'561618617212054','8c1f54c3d570a78fe6d325ab8401f487'
 end
 
